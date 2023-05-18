15f11bdec0007c1ab8b46494e6ff2367eb0e9ce5
Michał Sawicz (Saviq) <michal@sawicz.net>
2023-05-18 18:57:57 +0200

    units: add first graphics-test-tools tests

--- checkbox-provider-mir/manage.py
+++ checkbox-provider-mir/manage.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-# Copyright 2023 Canonical Ltd.
+# Copyright Canonical Ltd.
 # All rights reserved.
 
-"""Management script for the mir provider."""
+"""Management script for the Mir provider."""
 
 from plainbox.provider_manager import setup
 from plainbox.provider_manager import N_
 
 setup(
     name='checkbox-provider-mir',
-    namespace='com.canonical.qa.mir',
+    namespace='io.mir-server',
     version="0.1",
-    description=N_("Checkbox Provider for mir devices"),
+    description=N_("Checkbox Provider for testing graphics support and Mir"),
     gettext_domain='checkbox-provider-mir',
 )
