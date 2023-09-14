b5ec33d744ba29910ab45c3d327efe7cd9c0ee7d
Michał Sawicz (Saviq) <michal@sawicz.net>
2023-09-14 10:37:13 +0200

    mir-ci: use the gnome extension

--- bin/run_mir_ci.sh
+++ bin/run_mir_ci.sh
@@ -2,4 +2,7 @@
 
 cd $( dirname $( python3 -c 'import mir_ci; print(mir_ci.__file__)' ) )
 
+unset XKB_CONFIG_ROOT
+export XDG_RUNTIME_DIR=$( dirname $XDG_RUNTIME_DIR )
+
 exec python3 -m pytest "$@" -o cache_dir=$TMPDIR/pytest-cache
