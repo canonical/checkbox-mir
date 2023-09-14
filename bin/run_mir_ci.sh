0115d02a30d7b3a8518ae9a6c55472f95e0a10ea
Michał Sawicz (Saviq) <michal@sawicz.net>
2023-09-14 12:11:54 +0200

    mir-ci: fix mir-ci environment

--- bin/run_mir_ci.sh
+++ bin/run_mir_ci.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -eu
+
 cd $( dirname $( python3 -c 'import mir_ci; print(mir_ci.__file__)' ) )
 
 unset XKB_CONFIG_ROOT
-export XDG_RUNTIME_DIR=$( dirname $XDG_RUNTIME_DIR )
+export XDG_RUNTIME_DIR=/run/user/$UID
 
-exec python3 -m pytest "$@" -o cache_dir=$TMPDIR/pytest-cache
+exec python3 -m pytest "$@" -o cache_dir=/tmp/pytest-cache
