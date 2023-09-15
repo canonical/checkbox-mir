9f9798c2591a268d481f80d5794aee384b1423ae
Michał Sawicz (Saviq) <michal@sawicz.net>
2023-09-15 11:40:21 +0200

    mir-ci: ignore missing tools warnings

--- bin/run_mir_ci.sh
+++ bin/run_mir_ci.sh
@@ -7,4 +7,8 @@ cd $( dirname $( python3 -c 'import mir_ci; print(mir_ci.__file__)' ) )
 unset XKB_CONFIG_ROOT
 export XDG_RUNTIME_DIR=/run/user/$UID
 
-exec python3 -m pytest "$@" -o cache_dir=/tmp/pytest-cache
+exec python3 \
+    -m pytest \
+    -o cache_dir=/tmp/pytest-cache \
+    -o filterwarnings='ignore:.*missing tools.*' \
+    "$@"
