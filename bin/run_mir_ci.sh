ced0b821e5423d8ab7c4952b77af72457f3cddc7
Michał Sawicz <michal@sawicz.net>
2024-02-27 10:42:55 +0100

    mir-ci: fix LD_LIBRARY_PATH for blas/lapack

--- bin/run_mir_ci.sh
+++ bin/run_mir_ci.sh
@@ -6,6 +6,7 @@ cd $( dirname $( python3 -c 'import mir_ci; print(mir_ci.__file__)' ) )
 
 unset XKB_CONFIG_ROOT
 export XDG_RUNTIME_DIR=/run/user/$UID
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SNAP}/usr/lib/${SNAP_LAUNCHER_ARCH_TRIPLET}/blas:${SNAP}/usr/lib/${SNAP_LAUNCHER_ARCH_TRIPLET}/lapack
 
 exec python3 \
     -m pytest \
